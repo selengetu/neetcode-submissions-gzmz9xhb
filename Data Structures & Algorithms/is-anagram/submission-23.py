5734461475c6f350ae99beefabec1634b8e37ab0
Selenge Tulga <49024084+selengetu@users.noreply.github.com>
2026-04-06 16:14:40 -0500

    Add: is-anagram - submission-23

---
+++ Data Structures & Algorithms/is-anagram/submission-23.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False
+        
+        strS ,strT = {}, {}
+
+        for i in range(len(s)):
+            strS[s[i]] = 1+ strS.get(s[i], 0)
+            strT[t[i]] = 1+ strT.get(t[i], 0)
+        return strS == strT
