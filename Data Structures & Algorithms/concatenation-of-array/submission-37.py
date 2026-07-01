036d4623da6ba8f798640a8177af8dd55bb7ae35
Selenge Tulga <49024084+selengetu@users.noreply.github.com>
2026-07-01 11:17:26 -0500

    Add: concatenation-of-array - submission-37

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-37.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        
+        l = len(nums)
+        ans = [0] * 2 * l
+
+        for i, n in enumerate(nums):
+
+            ans[i] = ans[i+l] = n
+        
+        return ans
